4c0f919fa4f2cdaff00bfd646b87f446c59b5a09
Jens Feodor Nielsen <jens.nielsen@numerous.com>
2024-04-22 14:27:27 +0200

    chore: setup pre-commit

---
+++ tests/test_job.py
@@ -0,0 +1,11 @@
+import pytest
+from goer.job import Job
+
+
+@pytest.mark.asyncio
+async def test_job() -> None:
+    job = Job("my_job", ["echo hello world"])
+
+    result = await job.run()
+
+    assert result is True
